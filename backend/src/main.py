563122c901e4d88880ce2fc3d4d19b254cd6fe80
iddev5 <ayushbardhan5@gmail.com>
2024-01-14 01:06:38 +0530

    backend: Add video upload via post

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from fastapi import FastAPI, Depends
+from typing import Optional, Annotated
+from fastapi import FastAPI, Depends, Form, UploadFile
 from sqlmodel import SQLModel, select, Field, Session
 from .database import engine, db_session
 
@@ -11,8 +11,7 @@ class Video(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     desc: str
-    
-    
+    video: bytes
 
 
 @app.on_event("startup")
@@ -21,21 +20,28 @@ def startup():
 
 
 @app.post("/video")
-def add_video(name: str, desc: str, session: Session = Depends(db_session)):
-    session.add(Video(name=name, desc=desc))
+async def add_video(
+    name: Annotated[str, Form()],
+    desc: Annotated[str, Form()],
+    video: UploadFile,
+    session: Session = Depends(db_session),
+):
+    vid = Video(name=name, desc=desc, video=await video.read())
+    session.add(vid)
     session.commit()
-    return {"status": "success"}
+    session.refresh(vid)
+    return {"id": vid.id}
 
 
 @app.get("/video")
 def list_videos(session: Session = Depends(db_session)):
-    statement = select(Video).limit(50)
-    return session.exec(statement).all()
+    statement = select(Video.id, Video.name).limit(50)
+    res = session.exec(statement).all()
+    return [{"id": i.id, "name": i.name} for i in res]
 
 
 @app.get("/video/{id}")
 def get_video(id: int, session: Session = Depends(db_session)):
-    statement = select(Video).where(Video.id == id).limit(1)
-    return session.exec(statement).all()[0]
-
-
+    statement = select(Video).where(Video.id == id)
+    video = session.exec(statement).one()
+    return {"data": str(video.video)}
