3202d59a90e1a3ba58ab36cabecbaa7bf86fe370
iddev5 <ayushbardhan5@gmail.com>
2024-01-17 23:33:33 +0530

    backend: Add loan

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Depends, Form, UploadFile
 from sqlmodel import SQLModel, select, Field, Session
 from .database import engine, db_session
 
+PAGE_SIZE = 20
 
 app = FastAPI()
 
@@ -12,6 +13,13 @@ class Video(SQLModel, table=True):
     name: str
     desc: str
     video: bytes
+
+
+class Loan(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    desc: str
+    url: str
 
 
 @app.on_event("startup")
@@ -34,8 +42,8 @@ async def add_video(
 
 
 @app.get("/video")
-def list_videos(session: Session = Depends(db_session)):
-    statement = select(Video.id, Video.name).limit(50)
+def list_videos(page: int = 0, session: Session = Depends(db_session)):
+    statement = select(Video.id, Video.name).offset(PAGE_SIZE * page).limit(PAGE_SIZE)
     res = session.exec(statement).all()
     return [{"id": i.id, "name": i.name} for i in res]
 
@@ -45,3 +53,20 @@ def get_video(id: int, session: Session = Depends(db_session)):
     statement = select(Video).where(Video.id == id)
     video = session.exec(statement).one()
     return {"data": str(video.video)}
+
+
+@app.post("/loan")
+def add_loan(name: str, desc: str, url: str, session: Session = Depends(db_session)):
+    loan = Loan(name=name, desc=desc, url=url)
+    session.add(loan)
+    session.commit()
+    session.refresh(loan)
+    return {"id": loan.id}
+
+
+@app.get("/loan")
+def list_loans(page: int = 0, session: Session = Depends(db_session)):
+    statement = select(Loan).offset(PAGE_SIZE * page).limit(PAGE_SIZE)
+    res = session.exec(statement).all()
+    return res
+
